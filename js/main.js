9fef3e17afe7c9e70cca9c2ff078e17d66c2460f
Ravencer <ravencer880@gmail.com>
2021-01-30 18:01:05 +0700

    Выполнил второе обязательное задание

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 let money = 2500;
-let income = 1000;
-let addExpenses = 50;
-let deposit = 10;
-let misson = 1000;
-let period = 24;
-alert("Hello world!");
-console.log("Hello world!");
+let income = "фриланс";
+let addExpenses = "интернет, такси, коммуналка";
+let deposit = true;
+let misson = 55600;
+let period = 12;
+
+console.log(typeof money, typeof income, typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен " + period + " месяцев");
+console.log("Цель заработать " + misson + " рублей/долларов/гривен/юани");
+addExpenses = addExpenses.toLowerCase();
+console.log(addExpenses.split(","));
+let budgetDay = money / 30;
+console.log(budgetDay);
+
